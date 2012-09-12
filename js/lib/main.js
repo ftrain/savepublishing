ff839ade2192ec119de1fe784336dc9baa84067e
Paul Ford <ford@ftrain.com>
2012-09-12 10:08:32 -0400

    Refactoring into a tidier text cleaner. Clear I need to do more work measuring navs.

--- js/lib/main.js
+++ js/lib/main.js
@@ -1,12 +1,18 @@
 require.config({
-                   urlArgs:"bust=" + (new Date()).getTime(),
-                   paths:{
-                       'twidgets':'http://platform.twitter.com/widgets',
-                       'ui':'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min'
-                   }
-               });
+    urlArgs:"bust=" + (new Date()).getTime(),
+    paths:{
+        'twidgets':'http://platform.twitter.com/widgets',
+        'ui':'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min'
+    }
+});
 
 function loadCss(url) {
+    /**
+     *
+     * @type {HTMLElement}
+     *
+     * Inserts stylesheet into source.
+     */
     var link = document.createElement("link");
     link.type = "text/css";
     link.rel = "stylesheet";
@@ -19,133 +25,211 @@ function loadCss(url) {
     }
 }
 
-require(["jquery", "twidgets", "ui", "socialtext"], function ($) {
+/*
+
+ Extensions to native string.
+
+ */
+
+var trim = /^\s*(.*)\s*$/g;
+String.prototype.trimWhitespace = function () {
+    /**
+     * Trims whitespace.
+     *
+     * Depends on a regular expression in variable trim.
+     *
+     *  @return {String}
+     */
+    return this.replace(trim, '\$1');
+}
+
+var newlines = /[\n\r]/g;
+String.prototype.stripNewlines = function () {
+    /**
+     * Replaces newline with spaces.
+     *
+     * Depends on a regular expression in variable newLines.
+     */
+    return this.replace(newlines, ' ');
+}
+
+var para = /(.+?)SOCIALTEXT_BR\s*SOCIALTEXT_BR/g;
+String.prototype.twoBreaksToSocialtext = function () {
+    /**
+     * Wraps text before two occurrences of SOCIALTEXT_BR in a socialtext class.
+     *
+     * Depends on a regular expression in variable para.
+     *
+     *  @return {String}
+     */
+
+    return this.replace(para, '<span class="socialtext-line">\$1</span><br/><br/>\n\n');
+}
+
+var rebreak = /(.*?)SOCIALTEXT_BR/g;
+String.prototype.oneBreakToSocialtext = function () {
+    /**
+     * Wraps text before a break in a socialtext class.
+     *
+     * @return {String}
+     *
+     * Depends on a regular expression in variable rebreak.
+     */
+    return this.replace(rebreak, '<span class="socialtext-line">\$1</span><br/>');
+}
+
+String.prototype.wrapInSpan = function () {
+    /**
+     * Wraps text in a socialtext class.
+     *
+     * @return {String}
+     */
+
+    return '<span class="socialtext-text">' + this + '</span>';
+}
+
+var entities = /[&"><]/g;
+String.prototype.replaceEntities = function () {
+    /**
+     *
+     * @param char
+     * @return {String}
+     */
+
+    function replace(char) {
+        /**
+         * Inserts HTML entitities
+         *
+         * @param char a single character that is one of &"><
+         */
+        switch (char) {
+            case '&':
+                return '&amp';
+            case '"':
+                return '&quot;';
+            case '>':
+                return "&gt;"
+            case '<':
+                return "&lt;"
+        }
+    }
+
+    return this.replace(entities, function (a) {
+        return replace(a);
+    });
+}
+
+
+require(["jquery", "twidgets", "ui", "socialtext", "socialtext-styles"], function ($) {
+
 
     $(function () {
         loadCss('https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/ui-lightness/jquery-ui.css');
 
         // Get rid of scripts
 
-        window.onload = window.onunload = function () {};
+        window.onload = window.onunload = function () {
+        };
         $('script').remove();
+
+        var removeUntweetableHTML = function () {
+
+            console.log('Called removeUntweetableHTML');
+            //$('div[class~="nav"]').remove();
+            $('span,a').filter(function() {
+                console.log($(this).text().length);
+            });
+            $('font,abbr,cite,ins,del,q,s,code').replaceWith(function () {
+                return $(this).contents();
+            });
+            $('b,strong').replaceWith(function () {
+                return '*' + $(this).html() + '*';
+            });
+            $('i,em').replaceWith(function () {
+                return '_' + $(this).html() + '_';
+            });
+            $('br').replaceWith(function () {
+                return 'SOCIALTEXT_BR';
+            });
+        }
+
+        var reparseHTML = function () {
+            console.log('Called reparseHTML');
+
+            $('body').html($('body').html());
+        }
+
+
+        var allTextNodes = function () {
+
+            console.log('Called allTextNodes');
+
+            function getTextNodes(node) {
+
+                function clean(str, nodeToCheck) {
+                    var ret = str;
+
+                    if (nodeToCheck.parentNode.nodeName != 'PRE') {
+                        ret = ret.stripNewlines().trimWhitespace();
+                    }
+
+                    return ret.replaceEntities().twoBreaksToSocialtext().oneBreakToSocialtext().wrapInSpan();
+
+                }
+
+                if (node) {
+                    if (node.nodeType == 3) {
+                        var cleaned = clean(node.nodeValue, node);
+                        var el = $(cleaned)[0];
+                        node.parentNode.insertBefore(el, node);
+                        node.parentNode.removeChild(node);
+                    }
+                    else {
+                        for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+                            getTextNodes(node.childNodes[i]);
+
+                        }
+                    }
+                }
+            }
+
+            getTextNodes(document.body);
+        }
+
+
+        var fadeSomeElements = function () {
+            $('img,object,iframe,script,ins').fadeTo('fast', 0.3);
+        }
+        /* From Underscore */
+        var compose = function () {
+            var funcs = arguments;
+
+            return function () {
+
+                var args = arguments;
+                for (var i = funcs.length - 1; i >= 0; i--) {
+                    args = [funcs[i].apply(this, args)];
+                }
+                return args[0];
+            };
+        };
+
+        compose(fadeSomeElements, reparseHTML, allTextNodes, reparseHTML, removeUntweetableHTML)();
 
 
         // Fade stuff out
-        $('img,object,iframe,script,ins').fadeTo('fast', 0.3);
-        $('b,strong').replaceWith(function () {
-            return '*' + $(this).text().trim() + '*';
-        });
-        $('i,em').replaceWith(function () {
-            return '_' + $(this).text().trim() + '_';
-        });
-        $('span, a,abbr,cite,ins,del,q,s,code').replaceWith(function () {
-            return $(this).contents();
-        });
-        var html = $('body').html();
-        html = html.replace(/([^<>]+)<br[^>]?>\s*<br[^>]?>/gim, '<p>\$1</p>');
-        html = html.replace(/([^<>]+)<br[^>]?>/gim, '<div class="socialtext-break">\$1</div>');
-        $('body').html(html);
-        $('body').score(400);
-        $('.socialtext-scored').socialtext();
-        $('body').header();
+        /*
 
-        $('body').css({'margin-top':'225px'});
+         $('body').html(html);
+         $('body').score(400);
+         $('.socialtext-scored').socialtext();
+         $('body').header();
 
-        $('#socialtext-wrapper').css({'text-align':'center',
-                                         'width':'100%',
-                                         'position':'fixed',
-                                         'opacity':'1',
-                                         'line-height':'200%',
-                                         'z-index':'100000',
-                                         'font-size':'15px',
-                                         'background-color':'white',
-                                         'top':'210px'
-                                     });
+         $().applySocialtextStyles();
 
-        $('#socialtext-header').css({
-                                        'color':'black',
-                                        'background-color':'white',
-                                        'width':'600px',
-                                        'margin':'-210px auto 50px auto',
-                                        'font-family':"'Gill Sans','Helvetica Neue', 'Arial'",
-                                        'font-size':'15px',
-                                        'height':'150px',
-                                        'padding':'1em 1em 1em 1em',
-                                        'opacity':'.95',
-
-                                        'border':'10px solid #8bc',
-                                        'border-radius':'1em',
-                                        '-webkit-box-shadow':'11px 11px 19px rgba(50, 50, 50, 0.3)',
-                                        '-moz-box-shadow':'11px 11px 19px rgba(50, 50, 50, 0.3)',
-                                        'box-shadow':'11px 11px 19px rgba(50, 50, 50, 0.3)'
-                                    });
-
-        $('.socialtext-checkbox').css({'cursor':'pointer'});
-
-        $('.socialtext-checkbox:input').css({'cursor':'pointer'});
-
-        $('#socialtext-reset').css({
-                                       'text-align':'right',
-                                       'margin':'-1em -.5em 0 0'
-
-                                   });
-
-        $('#socialtext-wrapper').css({
-                                         'color':'#ccc',
-                                         'text-decoration':'none'
-                                     }
-        )
-        ;
-
-        $('#socialtext-slider')
-            .css({
-                     'width':'100%',
-                     '-webkit-box-shadow':'2px 5px 10px rgba(50, 50, 50, 0.3)',
-                     '-moz-box-shadow':'2px 2px 10px rgba(50, 50, 50, 0.3)',
-                     'box-shadow':'2px 2px 10px rgba(50, 50, 50, 0.3)'
-
-                 });
-
-        $('#socialtext-header h1')
-            .css({
-                     'font-size':'20px',
-                     'text-transform':'uppercase',
-                     'letter-spacing':'1em',
-                     'font-weight':'100',
-                     'margin-top':'-10px',
-                     'text-align':'center',
-                     'color':'#678c9a'
-                 });
-
-        $('#socialtext-footer')
-            .css({
-                     'font-size':'80%',
-                     'border-top':'1px dotted #bbb',
-                     'padding-top':'3px'
-                 });
-
-        $('a.socialtext-show')
-            .css({
-                     'background':'yellow',
-                     'color':'#0000cd',
-                     'margin-right':'.5em',
-                     'border-bottom':'1px solid white',
-                     'text-decoration':'none'
-                 });
-
-        $('a.socialtext-show:after').css({'content':'"\2605"'});
-
-        $('a.socialtext-hide').css({
-                                       'text-decoration':'none',
-                                       'background':'white',
-                                       'color':'silver',
-                                       'margin-right':'.5em'
-                                   });
-
-        $('.socialtext-scored').css({
-                                        'color':'#dc143c'
-                                    });
+         */
 
 
     });
-});
+})
+;
