065221481e2422be96acb9ea7432fb38a0fac701
Rusty Foster <rusty@kuro5hin.org>
2013-01-11 16:03:14 -0500

    Let's consider span a block element

--- htdocs/js/lib/savepublishing.js
+++ htdocs/js/lib/savepublishing.js
@@ -35,9 +35,9 @@
 
   BLOCKS = ['block', 'inline-block', 'table-cell', 'table-caption', 'list-item', 'none'];
 
-  BLOCK_ELEMENTS = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'BODY'];
+  BLOCK_ELEMENTS = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'BODY', 'SPAN'];
 
-  TEXTISH_ELEMENTS = ['SPAN', 'EM', 'B', 'STRONG', 'I', 'TT', 'ABBR', 'ACRONYM', 'BIG', 'CITE', 'CODE', 'DFN', 'LABEL', 'Q', 'SAMP', 'SMALL', 'SUB', 'SUP', 'VAR', 'DEL', 'INS', 'BR'];
+  TEXTISH_ELEMENTS = ['EM', 'B', 'STRONG', 'I', 'TT', 'ABBR', 'ACRONYM', 'BIG', 'CITE', 'CODE', 'DFN', 'LABEL', 'Q', 'SAMP', 'SMALL', 'SUB', 'SUP', 'VAR', 'DEL', 'INS', 'BR'];
 
   IRRELEVANT_ELEMENTS = ['IMG', 'OBJECT', 'EMBED', 'IFRAME', 'SCRIPT', 'INPUT', 'FORM', 'HEAD', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'STYLE', 'LINK'];
 
